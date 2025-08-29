54defc4097373161870e347884cbb27fb154fa99
Sumedh Barsagade <sumedh.barsagade@hotelkeyapp.com>
2025-08-29 20:54:10 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,7 +51,7 @@ export default function HomePage() {
             {/* Logo */}
             <div className="flex items-center space-x-2">
               <Bot className="h-8 w-8 text-purple-400" />
-              <span className="text-xl font-bold text-white">AutoAgent</span>
+              <span className="text-xl font-bold text-white">Web Automation</span>
             </div>
             
             {/* Social Links */}
